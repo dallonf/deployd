ec9c9f411d9f19c26bfb3d60f24e1e9bb70f5f3b
Dallon Feldner <dallonfdev@gmail.com>
2014-07-11 19:43:05 -0500

    Added failing test for network error reporting

--- test-app/public/test/misc.test.js
+++ test-app/public/test/misc.test.js
@@ -1,10 +1,10 @@
 describe('404s', function(){
   it('should not prevent a server from responding', function(done) {
     this.timeout(5000);
-    
+
     var tests = 50;
     var remaining = tests;
-    
+
     while(tests--) {
       dpd('/').post({foo: 'bar'}, function (res, err) {
         remaining--;
@@ -13,5 +13,19 @@ describe('404s', function(){
         }
       });
     }
+  });
+});
+
+describe('Network errors', function() {
+  it('should not return an empty string as the exception', function(done) {
+    _dpd.ajax('http://0.0.0.0:0000/does-not-exist', {
+      method: 'GET',
+      success: function() {
+        assert.fail("A request to http://0.0.0.0:0000/does-not-exist somehow succeeded");
+      },
+      error: function(result) {
+        expect(result).not.to.equal('');
+      }
+    })
   });
 });
